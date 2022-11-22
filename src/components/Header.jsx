320e8b761ce364a4102f35ba3f08807c9f6745b3
Prosper Success <osiletusuccess@gmail.com>
2022-11-22 19:53:27 +0100

    fixed navbar for smaller devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import { AiOutlineClose } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { motion } from "framer-motion";
 
+const variants = {
+  open: { opacity: 1, x: "100" },
+  closed: { opacity: 0, x: "-100%" },
+};
 const Header = () => {
-  const [active, setActive] = useState("#");
-
-  const activeClass = () => {
-    setActive(!active);
-  };
+  const [open, setOpen] = useState(false);
 
   //assigning location variable
   const location = useLocation();
@@ -21,16 +24,23 @@ const Header = () => {
   return (
     <div className="py-4">
       <div className="flex justify-between items-center">
-        <div className="links flex text-white ">
-          <li
+        <div className="links hidden md:flex text-white ">
+          <motion.li
             className={
               splitLocation[1] === ""
                 ? "px-2 mx-2 list-none bg-black opacity-[0.3] rounded-md py-2"
                 : "px-2 list-none py-2 hover:bg-black hover:opacity-[0.3] hover:rounded-md"
             }
+            initial={{ opacity: 0.9 }}
+            whileHover={{
+              scale: 1,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            whileInView={{ opacity: 0.3 }}
           >
             <Link to="/">Home</Link>
-          </li>
+          </motion.li>
           <li
             className={
               splitLocation[1] === "about"
@@ -59,7 +69,59 @@ const Header = () => {
             <Link to="contact">Contact</Link>
           </li>
         </div>
-        <h2 className="logo text-3xl text-white">Prosper</h2>
+        <h2 className="hidden md:flex logo text-3xl text-white">Prosper</h2>
+
+        <motion.div className="md:hidden -ml-[2rem] flex flex-col p-3 bg-slate-500 rounded-md opacity-[0.9]">
+          <div className="cursor-pointer" onClick={() => setOpen(!open)}>
+            {!open ? (
+              <GiHamburgerMenu className="w-8 text-2xl text-white my-2" />
+            ) : (
+              <AiOutlineClose className="w-8 text-2xl text-white my-2" />
+            )}
+          </div>
+          <div
+            className={!open ? "hidden" : "visible"}
+            animate={!open ? "closed" : "open"}
+            variants={variants}
+          >
+            <li
+              className={
+                splitLocation[1] === ""
+                  ? "px-2 my-2 list-none text-white -z-10 bg-black opacity-[0.3] rounded-md py-2"
+                  : "px-2 list-none py-2 text-white hover:bg-black hover:opacity-[0.3] hover:rounded-md"
+              }
+            >
+              <Link to="/">Home</Link>
+            </li>
+            <li
+              className={
+                splitLocation[1] === "about"
+                  ? "px-2 my-2 list-none bg-black opacity-[0.3] rounded-md py-2"
+                  : "px-2 list-none py-2  hover:bg-black hover:opacity-[0.3] rounded-md"
+              }
+            >
+              <Link to="about">About</Link>
+            </li>
+            <li
+              className={
+                splitLocation[1] === "projects"
+                  ? "px-2 my-2 list-none bg-black opacity-[0.3] rounded-md py-2"
+                  : "px-2 list-none py-2 hover:bg-black hover:opacity-[0.3] rounded-md"
+              }
+            >
+              <Link to="projects">Projects</Link>
+            </li>
+            <li
+              className={
+                splitLocation[1] === "contact"
+                  ? "px-2 my-2 list-none bg-black opacity-[0.3] rounded-md py-2"
+                  : "px-2 list-none py-2 hover:bg-black hover:opacity-[0.3] rounded-md"
+              }
+            >
+              <Link to="contact">Contact</Link>
+            </li>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
