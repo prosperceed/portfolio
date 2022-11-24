ff69541c6454c762b60b97b6858cf99953ce9b73
Prosper Success <osiletusuccess@gmail.com>
2022-11-25 00:46:07 +0100

    social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,19 @@ const Footer = () => {
   return (
     <div className="flex flex-col items-center py-8 border-t border-slate-700">
       <div className="socials flex gap-x-3">
-        <Link>
+        <Link target="_blank" to="https://twitter.com/kucozwa">
           <FaTwitter className="text-2xl text-cyan-400" />
         </Link>
-        <Link>
+        <Link target="_blank" to="https://web.facebook.com/prosper.succx/">
           <FaFacebookMessenger className="text-2xl text-purple-800" />
         </Link>
-        <Link>
+        <Link
+          target="_blank"
+          to="https://www.linkedin.com/in/prosper-kucozwa-success-2841b9216/"
+        >
           <FaLinkedin className="text-2xl text-cyan-400" />
         </Link>
-        <Link>
+        <Link target="_blank" to="https://github.com/prosperceed">
           <FaGithub className="text-2xl" />
         </Link>
       </div>
